8fc36433c0de418565068eab430559c1bbc541c8
Nasrullah-balghari <nasrullahbalghari676@gmail.com>
2022-09-24 21:49:44 +0500

    eid-calander

---
+++ app.js
@@ -0,0 +1,64 @@
+//Eid Qurban calander
+//first day
+var today = new Date();
+var firstDayEid = new Date("july 10,2022")
+var diff =  firstDayEid.getTime() - today.getTime() ;
+document.write("<b>"+"(1) First day of the Eid"+"<b>"+"<br>")
+document.write("-----------------------------"+"<br>")
+document.write("Days left: "+Math.floor(diff/(1000 * 60 * 60 * 24)));
+document.write("<br>")
+document.write("Hours left: "+Math.floor(diff/(1000 * 60 * 60)));
+document.write("<br>")
+document.write("Minutes left: "+Math.floor(diff/(1000 * 60 )));
+document.write("<br>")
+document.write("Seconds left: "+Math.floor(diff/(1000)));
+document.write("<br>")
+document.write("Milliseconds left: "+Math.floor(diff));
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+//second day
+var today = new Date();
+var firstDayEid = new Date("july 11,2022")
+var diff =  firstDayEid.getTime() - today.getTime() ;
+document.write("<b>"+"(2) Second day of the Eid"+"<b>"+"<br>")
+document.write("---------------------------------"+"<br>")
+document.write("Days left: "+Math.floor(diff/(1000 * 60 * 60 * 24)));
+document.write("<br>")
+document.write("Hours left: "+Math.floor(diff/(1000 * 60 * 60)));
+document.write("<br>")
+document.write("Minutes left: "+Math.floor(diff/(1000 * 60 )));
+document.write("<br>")
+document.write("Seconds left: "+Math.floor(diff/(1000)));
+document.write("<br>")
+document.write("Milliseconds left: "+Math.floor(diff));
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+//third day
+var today = new Date();
+var firstDayEid = new Date("july 12,2022")
+var diff =  firstDayEid.getTime() - today.getTime() ;
+document.write("<b>"+"(3) Third day of the Eid"+"<b>"+"<br>")
+document.write("-------------------------------"+"<br>")
+document.write("Days left: "+Math.floor(diff/(1000 * 60 * 60 * 24)));
+document.write("<br>")
+document.write("Hours left: "+Math.floor(diff/(1000 * 60 * 60)));
+document.write("<br>")
+document.write("Minutes left: "+Math.floor(diff/(1000 * 60 )));
+document.write("<br>")
+document.write("Seconds left: "+Math.floor(diff/(1000)));
+document.write("<br>")
+document.write("Milliseconds left: "+Math.floor(diff));
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+
+
+
+
